4b4c0f34f718e1921c9001f0ce8ee650673d0e4b
nganuaja <nganuaja123@gmail.com>
2024-09-28 13:58:19 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,13 @@ window.addEventListener('load', function() {
     console.log("Autoplay was prevented. User interaction required.");
   });
 });
+
+// Deteksi klik pertama atau sentuhan pengguna untuk memulai musik
+window.addEventListener('click', function() {
+  var audio = document.getElementById("background-music");
+  if (audio.paused) {
+    audio.play().catch(error => {
+      console.log("Gagal memutar musik:", error);
+    });
+  }
+}, { once: true }); // Event hanya sekali di-trigger
